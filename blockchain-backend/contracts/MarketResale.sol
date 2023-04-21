dbb087b03985ce6565663b2f6074a9dd90106c2a
0xsourin <sourin07@gmail.com>
2023-04-21 23:20:34 +0530

    Updated

--- blockchain-backend/contracts/MarketResale.sol
+++ blockchain-backend/contracts/MarketResale.sol
@@ -41,13 +41,54 @@ contract NFTMarketResell is ERC721Enumerable, Ownable, ReentrancyGuard {
 
     constructor(ERC721Enumerable _nft) {
         owner = payable(msg.sender);
-        nft = _nft;
+        nft = ERC721Enumerable(_nft);
     }
 
     function listSale(
         uint256 tokenId,
         uint256 price
     ) public payable nonReentrant {
-        require(nft.ownerOf(tokenId) == msg.sender,"you are not the owner");
+        require(nft.ownerOf(tokenId) == msg.sender, "you are not the owner");
+        require(vaultItems[tokenId].tokenId == 0, "Token already listed");
+        require(price > 0, "Price must be greater than 0");
+        require(msg.value == listingFee, "Please pay the listing fee");
+
+        _itemsIds.increment();
+        uint256 itemId = _itemsIds.current();
+
+        vaultItems[itemId] = List(
+            itemId,
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+        nft.transferFrom(msg.sender, address(this), tokenId);
+
+        emit NFTListCreated(
+            itemId,
+            tokenId,
+            msg.sender,
+            address(this),
+            price,
+            false
+        );
+    }
+
+    //Todo: Now gonna deal with the buyer
+    function buyNFT(uint256 tokenId) public payable nonReentrant {
+        List storage myNFT = vaultItems[tokenId];
+        uint256 price = myNFT.price;
+        uint256 
+        require(msg.value == price, "Pay the correct price");
+
+        myNFT.seller.transfer(msg.value);
+
+        nft.transferFrom(address(this), msg.sender, tokenId);
+
+        myNFT.sold = true;
+
+        delete vaultItems[tokenId];
     }
 }
