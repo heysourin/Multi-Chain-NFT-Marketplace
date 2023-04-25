08f6d7977611087118f192e0bb3c351afe5f20fb
0xsourin <sourin07@gmail.com>
2023-04-25 20:07:37 +0530

    Updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,52 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const connectWallet = async () => {
+    // setLoading(true);
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Metamask has found!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+      setLoadingState("Not-loaded");
+    } catch (err) {
+      console.error(err.message);
+      setLoadingState("Not-loaded");
+    }
+  };
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Pixel NFT Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/" className="mr-4 text-blue-500">
+            Home
+          </Link>
+          <Link href="/create-nft" className="mr-6 text-blue-500">
+            Sell NFT
+          </Link>
+          <Link href="/my-nfts" className="mr-6 text-blue-500">
+            My NFTs
+          </Link>
+          <Link href="/dashboard" className="mr-6 text-blue-500">
+            Dashboard
+          </Link>
+          <button onClick={connectWallet}>Connect Wallet</button>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- prev-indx.js
+++ prev-indx.js
@@ -15,6 +15,7 @@ export default function Home() {
     connectWallet();
     checkIfWalletIsConnected();
     loadNFTs();
+    // buyNft(nfts[0]);
   }, []);
 
   const checkIfWalletIsConnected = async () => {
@@ -41,7 +42,7 @@ export default function Home() {
       setLoadingState("Not-loaded");
     } catch (error) {
       console.error(error.message);
-      // setLoading(false);
+      setLoading(false);
     }
   };
 
@@ -69,7 +70,6 @@ export default function Home() {
   };
 
   async function loadNFTs() {
-    /* create a generic provider and query for unsold market items */
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const contract = new ethers.Contract(
       marketplaceAddress,
@@ -78,10 +78,6 @@ export default function Home() {
     );
     const data = await contract.fetchMarketItems();
 
-    /*
-     *  map over items returned from smart contract and format
-     *  them as well as fetch their token metadata
-     */
     const items = await Promise.all(
       data.map(async (i) => {
         const tokenUri = await contract.tokenURI(i.tokenId);
@@ -97,18 +93,20 @@ export default function Home() {
           name: meta.name,
           description: meta.description,
         };
-        console.log(item);
+        // console.log(item);
         return item;
       })
     );
-    setNfts(items);
+    setNfts({ items });
     setLoadingState("loaded");
   }
+
   async function buyNft(nft) {
-    /* needs the user to sign the transaction, so will use Web3Provider and sign it */
-    const web3Modal = new Web3Modal();
-    const connection = await web3Modal.connect();
-    const provider = new ethers.providers.Web3Provider(connection);
+    // if (!nft) {
+    //   console.error('NFT is undefined')
+    //   return
+    // }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const contract = new ethers.Contract(
       marketplaceAddress,
@@ -116,47 +114,54 @@ export default function Home() {
       signer
     );
 
-    /* user will be prompted to pay the asking proces to complete the transaction */
-    const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
-    const transaction = await contract.createMarketSale(nft.tokenId, {
-      value: price,
-    });
-    await transaction.wait();
-    loadNFTs();
+    // for (let i = 0; i < nfts.length; i++) {
+    //   nft = nfts[i];
+    // }
+    console.log(nft);
+
+    // console.log(await nft.price);
+    // const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+    // const transaction = await contract.createMarketSale(nft.tokenId, {
+    //   value: price,
+    // });
+    // await transaction.wait();
+    // loadNFTs();
   }
+
   if (loadingState === "loaded" && !nfts.length)
     return <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>;
-  return (
-    <div className="flex justify-center">
-      <div className="px-4" style={{ maxWidth: "1600px" }}>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {nfts.map((nft, i) => (
-            <div key={i} className="border shadow rounded-xl overflow-hidden">
-              <img src={nft.image} />
-              <div className="p-4">
-                <p
-                  style={{ height: "64px" }}
-                  className="text-2xl font-semibold"
-                >
-                  {nft.name}
-                </p>
-                <div style={{ height: "70px", overflow: "hidden" }}>
-                  <p className="text-gray-400">{nft.description}</p>
-                </div>
-              </div>
-              <div className="p-4 bg-black">
-                <p className="text-2xl font-bold text-white">{nft.price} ETH</p>
-                <button
-                  className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
-                  onClick={() => buyNft(nft)}
-                >
-                  Buy
-                </button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+
+  // return (
+    // <div className="flex justify-center">
+    //   <div className="px-4" style={{ maxWidth: "1600px" }}>
+    //     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+    //       {nfts.map((nft, i) => (
+    //         <div key={i} className="border shadow rounded-xl overflow-hidden">
+    //           <img src={nft.image} />
+    //           <div className="p-4">
+    //             <p
+    //               style={{ height: "64px" }}
+    //               className="text-2xl font-semibold"
+    //             >
+    //               {nft.name}
+    //             </p>
+    //             <div style={{ height: "70px", overflow: "hidden" }}>
+    //               <p className="text-gray-400">{nft.description}</p>
+    //             </div>
+    //           </div>
+    //           <div className="p-4 bg-black">
+    //             <p className="text-2xl font-bold text-white">{nft.price} ETH</p>
+    //             <button
+    //               className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
+    //               onClick={() => buyNft(nft)}
+    //             >
+    //               Buy
+    //             </button>
+    //           </div>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </div>
+  // );
 }
